5d7b291d51601002af3605ac18f5aa3f24e3a27d
Russell Day <russell.day.coder@gmail.com>
2015-07-23 21:50:30 +0100

    Initial project outline

---
+++ StringInterpolation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

08026110b94285a4890c21671cf5791f9ba5af4a
Russell Day <russell.day.coder@gmail.com>
2015-07-24 23:40:38 +0100

    Added beginning of exception filters tests and a mock currency exchange. Not sure if this example is the best way to illustrate exception filters.

---
+++ CSharpSixTour.Tests/ExceptionFiltersTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpSixTour.Domain;
+
+namespace CSharpSixTour.Tests
+{
+    [TestClass]
+    public class ExceptionFiltersTests
+    {
+        [TestMethod]
+        public void CSharp5_ExceptionFiltering()
+        {
+            try
+            {
+                Person person = new Person {
+                    FirstName = "Last",
+                    LastName = "First",
+                    Salary = new Salary {
+                        Currency = Currency.GBP,
+                        Amount = 1000
+                    }
+                };
+
+                MockCurrencyExchange exchange = new MockCurrencyExchange();
+
+                var dollars = exchange.Convert(person.Salary.Currency, Currency.USD, person.Salary.Amount);
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}

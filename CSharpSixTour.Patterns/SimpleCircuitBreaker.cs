14ea97031712e046b39ae261c023c0f0e54b970a
Russell Day <russell.day.coder@gmail.com>
2015-08-04 16:51:42 +0100

    Added SimpleCircuitBreakerPattern with some tests.

---
+++ CSharpSixTour.Patterns/SimpleCircuitBreaker.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace CSharpSixTour.Patterns
+{
+    public class SimpleCircuitBreaker<T>
+    {
+        private readonly T Target;
+
+        private readonly SimpleCircuitBreakerConfig Config;
+
+        private readonly Predicate<Exception> ExceptionFilter;
+
+        private int TryCounter { get; set; }
+
+        public CircuitBreakerState State { private set; get; }
+
+        public Exception LastException { get; set; }
+
+        public SimpleCircuitBreaker(T target, SimpleCircuitBreakerConfig config, Predicate<Exception> exceptionFilter = null)
+        {
+            if(target == null)
+            {
+                throw new ArgumentNullException(nameof(target));
+            }
+            if(config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            Target = target;
+            Config = config;
+            ExceptionFilter = exceptionFilter;
+            State = CircuitBreakerState.Open;
+        }
+
+        public U Call<U>(Func<T, U> action)
+        {
+            TryCounter++;
+
+            if (State == CircuitBreakerState.Closed)
+            {
+                if (TryCounter > Config.RetryThreshold)
+                {
+                    Open();
+                }
+                else
+                {
+                    throw LastException;
+                }
+            }
+
+            try
+            {
+                U returnValue = action.Invoke(Target);
+
+                Open();
+
+                return returnValue;
+            }
+            catch (Exception e) when ((ExceptionFilter?.Invoke(e)).GetValueOrDefault(true))
+            {
+                if (TryCounter > Config.Threshold)
+                {
+                    Close(e);
+                }
+
+                throw;
+            }
+        }
+
+        public void Call(Action<T> action)
+        {
+            Call(t =>
+            {
+                action.Invoke(t);
+                return 0;
+            });
+        }
+
+        private void Open()
+        {
+            TryCounter = 0;
+            State = CircuitBreakerState.Open;
+        }
+
+        private void Close(Exception exception)
+        {
+            State = CircuitBreakerState.Closed;
+            LastException = exception;
+            TryCounter = 0;
+        }
+    }
+
+    public class SimpleCircuitBreakerConfig
+    {
+        public int Threshold { get; set; }
+        public int RetryThreshold { get; set; }
+    }
+
+    public enum CircuitBreakerState
+    {
+        Open,
+        Closed
+    }
+}
---
+++ CSharpSixTour.Tests/SimpleCircuitBreakerTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using CSharpSixTour.Patterns;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace CSharpSixTour.Tests
+{
+    [TestClass]
+    public class SimpleCircuitBreakerTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Ctor_NullTarget_ThrowsArgumentNull()
+        {
+            // Arrange
+            SimpleCircuitBreaker<TestClass> cb = new SimpleCircuitBreaker<TestClass>(null, new SimpleCircuitBreakerConfig());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentNullException))]
+        public void Ctor_NullConfig_ThrowsArgumentNull()
+        {
+            // Arrange
+            SimpleCircuitBreaker<TestClass> cb = new SimpleCircuitBreaker<TestClass>(new TestClass(), null);
+        }
+
+        [TestMethod]
+        public void Ctor_Nothing_IsOpenByDefault()
+        {
+            // Arrange
+            SimpleCircuitBreaker<TestClass> cb = new SimpleCircuitBreaker<TestClass>(new TestClass(), new SimpleCircuitBreakerConfig());
+
+            // Assert
+            cb.State.ShouldBe(CircuitBreakerState.Open);
+        }
+
+        [TestMethod]
+        public void Call_DoNothing_DoesNothing()
+        {
+            // Arrange
+            SimpleCircuitBreaker<TestClass> cb = new SimpleCircuitBreaker<TestClass>(new TestClass(),
+                new SimpleCircuitBreakerConfig {Threshold = 0});
+
+            // Act
+            cb.Call(tc => tc.DoNothing());
+
+            // Assert
+            cb.State.ShouldBe(CircuitBreakerState.Open);
+            cb.LastException.ShouldBe(null);
+        }
+
+        [TestMethod]
+        public void Call_ThrowException_ClosesCircuit()
+        {
+            // Arrange
+            SimpleCircuitBreaker<TestClass> cb = new SimpleCircuitBreaker<TestClass>(new TestClass(),
+                new SimpleCircuitBreakerConfig {Threshold = 0});
+            Exception exception = null;
+
+            // Act
+            try
+            {
+                cb.Call(tc => { tc.ThrowException(); });
+            }
+            catch (Exception e)
+            {
+                exception = e;
+            }
+
+            // Assert
+            cb.State.ShouldBe(CircuitBreakerState.Closed);
+            exception.ShouldNotBe(null);
+            exception.Message.ShouldBe("fake exception");
+        }
+
+        [TestMethod]
+        public void Call_WithExceptionFilter_IgnoresException()
+        {
+            // Arrange
+            SimpleCircuitBreaker<TestClass> cb = new SimpleCircuitBreaker<TestClass>(new TestClass(),
+                new SimpleCircuitBreakerConfig {Threshold = 0}, exception => false);
+
+            // Act
+            try
+            {
+                cb.Call(tc => tc.ThrowException());
+            }
+            catch (Exception)
+            {
+                
+            }
+
+            // Assert
+            cb.State.ShouldBe(CircuitBreakerState.Open);
+            cb.LastException.ShouldBe(null);
+        }
+    }
+
+    public class TestClass
+    {
+        public void DoNothing()
+        {
+            
+        }
+
+        public void ThrowException()
+        {
+            throw new Exception("fake exception");
+        }
+    }
+}

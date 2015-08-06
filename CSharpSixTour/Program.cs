73655226ed31c612da8b7a8a3cc358b24e9d52ed
Russell Day <russell.day.coder@gmail.com>
2015-08-06 16:35:13 +0100

    Added a console application as a starting point for the tutorial. Will begin with auto-property initialisers and using static.

---
+++ CSharpSixTour/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharpSixTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyContrivedClass myContrivedClass = new MyContrivedClass();
+
+            myContrivedClass.PrintFoo();
+        }
+
+        class MyContrivedClass
+        {
+            private readonly string InternalFoo;
+
+            public MyContrivedClass()
+            {
+                InternalFoo = "Bar";
+            }
+
+            public void PrintFoo()
+            {
+                Console.WriteLine(InternalFoo);
+            }
+        }
+    }
+}
